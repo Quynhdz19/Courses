9f343a6874f8f7482b0ce74b495eadc2e737bdf1
Truong Pham Xuan <xuantruonglk02@gmail.com>
2024-09-07 21:37:44 +0700

    fix css video page

--- src/views/pages/courses/detail/videoVideoDetail.js
+++ src/views/pages/courses/detail/videoVideoDetail.js
@@ -1,18 +1,11 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { Avatar, Button, Col, Divider, Layout, List, Menu, Progress, Row } from 'antd'
-import ReactHlsPlayer from 'react-hls-player'
-import { CCard, CCardBody, CCardTitle } from '@coreui/react'
-import CourseDetailModuleCollapse from 'src/views/pages/courses/detail/CollapseModule'
-import { CKEditor } from '@ckeditor/ckeditor5-react'
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-]
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import { CCard, CCardBody, CCardTitle } from '@coreui/react'
+import { Col, Row } from 'antd'
+import React, { useRef } from 'react'
+import ReactHlsPlayer from 'react-hls-player'
+import CourseDetailModuleCollapse from 'src/views/pages/courses/detail/CollapseModule'
+import './videoVideoDetail.css'
 
 const VideoDetail = () => {
   const playerRef = useRef(null)
@@ -33,13 +26,76 @@ const VideoDetail = () => {
           id: 1,
           name: 'Bài 01: Nhập môn',
         },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+      ],
+    },
+    {
+      name: 'Module 01: Nhập Môn',
+      lessons: [
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+      ],
+    },
+    {
+      name: 'Module 01: Nhập Môn',
+      lessons: [
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
+        {
+          id: 1,
+          name: 'Bài 01: Nhập môn',
+        },
       ],
     },
   ]
+
   return (
-    <div className="form-container">
-      <Row>
-        <Col span={18}>
+    <div className="video-card">
+      <Row gutter={16}>
+        <Col span={16}>
           <ReactHlsPlayer
             src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
             hlsConfig={{
@@ -62,13 +118,12 @@ const VideoDetail = () => {
             }}
           />
         </Col>
-        <Col span={6}>
-          <CCard className={'border-light left-menu-video'}>
+        <Col span={8}>
+          <CCard className="border-light overflow-auto">
             <CCardBody className="d-grid gap-2">
               <CCardTitle>
                 <strong>Bài Học</strong>
               </CCardTitle>
-
               {modules.map((module, index) => (
                 <CourseDetailModuleCollapse key={index} module={module} />
               ))}
