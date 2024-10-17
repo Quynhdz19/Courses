3140614ddf71b25a9b6e049f8afa77dffa9fbe38
Phuc-Du <hunterofdark352@gmail.com>
2024-10-17 11:11:34 +0700

    feat: refresh access token

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,28 +1,83 @@
 import axios from 'axios'
 import { jwtDecode } from 'jwt-decode'
 import store from 'src/redux/store'
-import { logout } from '../redux/modules/authSlice'
+import { logout, setAuthAccessToken } from '../redux/modules/authSlice'
+
+const refreshTokenInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_API_URL,
+})
 
 const axiosInstance = axios.create({
   timeout: 10000,
   baseURL: import.meta.env.VITE_BASE_API_URL,
 })
 
+let isRefreshing = false
+const refreshSubscribers = []
+
+const subscribeTokenRefresh = (cb) => {
+  refreshSubscribers.push(cb)
+}
+
+const onRefreshed = (newAccessToken) => {
+  refreshSubscribers.forEach((cb) => cb(newAccessToken))
+  refreshSubscribers.length = 0
+}
+
+const onFailedRefreshed = (error) => {
+  refreshSubscribers.forEach((cb) => cb(null, error))
+  refreshSubscribers.length = 0
+}
+
 axiosInstance.interceptors.request.use(
-  (config) => {
+  async (config) => {
     const { auth } = store.getState()
     const accessToken = auth.accessToken
+    const refreshToken = auth.refreshToken
 
     if (accessToken) {
       const currentDate = new Date()
       const decodedAccessToken = jwtDecode(accessToken)
 
-      if (decodedAccessToken?.exp > currentDate.getTime() / 1000) {
-        config.headers['Authorization'] = `Bearer ${accessToken}`
-      } else {
-        store.dispatch(logout())
-        throw new axios.Cancel('Token expired, logging out...')
+      if (decodedAccessToken?.exp <= currentDate.getTime() / 1000) {
+        if (!isRefreshing) {
+          isRefreshing = true
+
+          try {
+            const response = await refreshTokenInstance.post('/auth/refresh-token', {
+              refreshToken: refreshToken,
+            })
+
+            const newAccessToken = response.data.accessToken
+
+            store.dispatch(setAuthAccessToken(newAccessToken))
+
+            config.headers['Authorization'] = `Bearer ${newAccessToken}`
+
+            isRefreshing = false
+
+            onRefreshed(newAccessToken)
+          } catch (error) {
+            isRefreshing = false
+            store.dispatch(logout())
+            onFailedRefreshed(error)
+            return Promise.reject(error)
+          }
+        }
+
+        return new Promise((resolve, reject) => {
+          subscribeTokenRefresh((newAccessToken, error) => {
+            if (error) {
+              reject(error)
+            } else {
+              config.headers['Authorization'] = `Bearer ${newAccessToken}`
+              resolve(config)
+            }
+          })
+        })
       }
+
+      config.headers['Authorization'] = `Bearer ${accessToken}`
     }
 
     return config
